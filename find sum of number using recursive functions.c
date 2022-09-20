6cfaf449f6f24c21618815fb4f1a0ebd9f24b97e
Faheem Firdous <faheemfirdous77@gmail.com>
2022-09-20 21:31:27 +0530

    Create find sum of number using recursive functions.c

---
+++ find sum of number using recursive functions.c
@@ -0,0 +1,21 @@
+// find sum of natural elements using recursive functions
+int sum_nat(int);
+int main()
+{
+	int n,result;
+	printf("Enter the number: ");
+	scanf("&d", &n);
+	result = sum_nat(n);
+	printf("The sum till %d natural number is: %d",n,result);
+}
+int sum_nat(int n)
+{
+	if(n==0)
+	{
+		return 0;
+	}
+	else
+	{
+		return(n + sum_nat(n-1));
+	}
+}
