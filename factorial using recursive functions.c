bead44af0518c63f0f2d8604b28fc5a0b05f6228
Faheem Firdous <faheemfirdous77@gmail.com>
2022-09-20 21:28:28 +0530

    Create factorial using recursive functions.c

---
+++ factorial using recursive functions.c
@@ -0,0 +1,28 @@
+// factorial using recursive functions
+int factorial(int);
+int main()
+{
+	int n,r;
+	printf("Enter the numnber: ");
+	scanf("%d",&n);
+	if(n<0)
+	{
+		printf("Factorial does not exsit.");
+	}
+	else
+	{
+		r = factorial(n);
+		printf("The factorial of the number is: %d",r);
+	}
+}
+int factorial(int n)
+{
+	if(n==1)
+    {
+        return(1);
+    }
+    else
+    {
+        return(n * factorial(n-1));
+    }
+}
