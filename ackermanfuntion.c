127410d9cb75e48178035394ac04b126156a06ac
Faheem Firdous <faheemfirdous77@gmail.com>
2022-09-20 22:29:25 +0530

    Create ackermanfuntion.c

---
+++ ackermanfuntion.c
@@ -0,0 +1,25 @@
+// ackerman function
+#include<stdio.h>
+int ack(int,int);
+int main()
+{
+	int m,n,a;
+	printf("Enter the value of M and N: ");
+	scanf("%d %d",&m,&n);
+	printf("The ackerman function on %d %d is %d.",m,n,ack(m,n));
+}
+int ack(int m,int n)
+{
+	if(m=0)
+	{
+		return n+1;
+	}
+	else if(m>0 && n==0)
+	{
+		return ack(m-1,1);
+	}
+	else if(m>0 && n>0)
+	{
+		return ack(m-1,n-1);
+	}
+}
