b5440dcb17cbd48d3e0d700c2f5bde1ba56a9d14
Faheem Firdous <faheemfirdous77@gmail.com>
2022-09-20 22:48:48 +0530

    Update ackermanfuntion.c

--- ackermanfuntion.c
+++ ackermanfuntion.c
@@ -3,16 +3,16 @@
 int ack(int,int);
 int main()
 {
-	int m,n,a;
+	int m,n;
 	printf("Enter the value of M and N: ");
 	scanf("%d %d",&m,&n);
 	printf("The ackerman function on %d %d is %d.",m,n,ack(m,n));
 }
 int ack(int m,int n)
 {
-	if(m=0)
+	if(m==0)
 	{
-		return n+1;
+		return n+1 ;
 	}
 	else if(m>0 && n==0)
 	{
@@ -20,6 +20,6 @@ int ack(int m,int n)
 	}
 	else if(m>0 && n>0)
 	{
-		return ack(m-1,n-1);
+		return ack(m-1, ack(m,n-1));
 	}
 }
