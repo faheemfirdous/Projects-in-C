0d222d19a2b22db3ee99dd917c764b5607e33b23
Faheem Firdous <faheemfirdous77@gmail.com>
2022-09-20 21:31:50 +0530

    Create factorial using functions.c

---
+++ factorial using functions.c
@@ -0,0 +1,26 @@
+// factorial using functions
+int factorial(int);
+int main()
+{
+	printf("Enter the numnber: ");
+	int n,r;
+	scanf("%d",&n);
+	if(n<0)
+	{
+		printf("Factorial does not exsit.");
+	}
+	else
+	{
+		r = factorial(n);
+		printf("The factorial of the number is: %d",r);
+	}
+}
+int factorial(int n)
+{
+	int ik,fact=0;
+	for(ik =0;ik<=fact;ik++)
+	{
+		fact *= ik;
+	}
+	return(fact);
+}
