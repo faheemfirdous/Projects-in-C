f9196bf6d5d5017c3f3e4238c70a73530ad395fe
Faheem Firdous <faheemfirdous77@gmail.com>
2022-09-20 21:29:01 +0530

    Create sort an array using functions.c

---
+++ sort an array using functions.c
@@ -0,0 +1,35 @@
+// sort an array using functions
+int sort(int array[], int);
+int main()
+{
+	int a,b;
+	int array[100];
+	printf("Enter the size of the array: ");
+	scanf("%d",&a);
+	printf("Enter the elements:\n");
+	for(b=0;b<a;b++)
+	{
+		scanf("%d",&array[b]);
+	}
+	sort(array,a);
+}
+int sort(int array[],int a)
+{
+    int i,j;
+    for(int i=0;i<a;i++)
+    {
+        for(j=0;j<a-1;j++)
+        {
+            if(array[j]>array[j+1])
+            {
+                int temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
+            }
+        }
+    }
+    for(i=0;i<a;i++)
+    {
+        printf("%d ",array[i]);
+    }
+}
